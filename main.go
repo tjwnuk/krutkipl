1aeae34c6b2313ca944ca6c439494488ae785709
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-01-24 18:56:09 +0100

    gin hello world

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Cześć!")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "witaj świecie")
+	})
+
+	router.Run()
 }
