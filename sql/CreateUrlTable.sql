63d1af1240bb180b4a98100c543d3f3a63bd9cad
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-02-08 02:28:02 +0100

    remove sql/ folder

--- sql/CreateUrlTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS Url (
-    ID INTEGER PRIMARY KEY NOT NULL,
-    CreatedAt TEXT,
-    UpdatedAt TEXT,
-    DeletedAt TEXT,
-    OriginalURL TEXT,
-    Token TEXT,
-    ShortenedURL TEXT
-)
