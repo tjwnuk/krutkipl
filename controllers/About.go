5668e2535aaf0481d85924fdf8b84a44fa7474cf
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-02-24 00:12:52 +0100

    about page

--- controllers/About.go
+++ controllers/About.go
@@ -1,15 +1,34 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"krutki.pl/models"
+)
 
 func (ct Controller) AboutHandler(c *gin.Context) {
-	currentUser, exists := c.Get("User")
+
+	var userString string
+	_, exists := c.Get("User")
+	var user *models.User
 
 	if exists {
-		c.JSON(200, currentUser)
-	} else {
-		c.JSON(200, gin.H{
-			"msg": "you are not logged in",
-		})
+		user = c.Keys["User"].(*models.User)
 	}
+
+	if exists {
+		userString += "ID:       " + strconv.Itoa(int(user.ID)) + "; "
+		userString += "Username: " + user.Username + "; "
+		userString += "Rank:     " + user.Rank + "; "
+	} else {
+		userString = "not logged in"
+	}
+
+	c.HTML(http.StatusOK, "pages/about", gin.H{
+		"User":       user,
+		"UserString": userString,
+		"title":      "About this site",
+	})
 }
